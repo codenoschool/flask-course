1d4e491dbe6c99d0983afae3279df6c2fad2d320
codenoschool <codenoschool@gmail.com>
2017-12-22 05:45:15 -0600

    Error Handler
    The project handle-error was modified and now works correctly.
    
    Changed the name of the variable "admin" to "admin_user" to avoid names
    confflict.
    
    @codenoschool

--- handle-errors/app.py
+++ handle-errors/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, abort, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
-admin = ""
+admin_user = ""
 
 @app.route("/")
 def index():
@@ -10,19 +10,19 @@ def index():
     return "Hello World!"
 
 @app.route("/admin")
-def protected_page():
-    if not admin:
+def admin():
+    if not admin_user:
         abort(403)
 
-    return "Welcome " + admin
+    return "Welcome {}!".format(admin_user)
 
 @app.errorhandler(404)
-def page_not_found(error):
+def page_not_found(err):
 
     return render_template("page_not_found.html"), 404
 
 @app.errorhandler(403)
-def forbidden(error):
+def forbidden(err):
 
     return render_template("forbidden.html"), 403
 
